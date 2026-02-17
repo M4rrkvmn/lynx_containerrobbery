ec1b182cff7ee3cb827c199a08886832db525d3d
Mrk. <m4rk1vmn@gmail.com>
2026-02-17 22:20:26 +0100

    Update V1.2 [FIX SQL]

--- import_me.sql
+++ import_me.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `lynx_containerrobbery` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `identifier` varchar(255) NOT NULL,
-    `xp` int(11) NOT NULL,
+    `xp` int(11) NOT NULL DEFAULT '0',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
